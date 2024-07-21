35da8423dce0adacfdeab36140b4c400213e6a47
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-21 09:24:39 +0100

    34

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,13 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-    name //undefined
-    age = 30;
-    fullname = "Salesforce poland";
-    showData = false;
-    details = {
-        name: "dummy",
-        area: "Earth"
-    }
-    userList = ["A", "B", "C"]
+   fullname = "Dev Training"
 }
