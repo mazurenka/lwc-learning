497ada09570b8ecdf8cd089638a3613394a94a3d
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-21 10:14:31 +0100

    35

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,13 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
    fullname = "Dev Training"
+   title = "aura"
+
+   onchangeHandler(event) {
+      this.title = event.target.value
+   }
+
+
+   
+
 }
