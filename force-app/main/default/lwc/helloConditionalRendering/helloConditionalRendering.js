3dac94af5702224b9f348e547a1c9618d05c3f44
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-22 10:29:48 +0100

    39

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,7 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
     isVisible = false
+    name
     handleClick() {
         this.isVisible = true
+    }
+
+    changeHandler(event) {
+        this.name = event.target.value
+    }
+    get helloMethod() {
+        return this.name === "Sorry"
     }
 }
