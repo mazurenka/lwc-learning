4004ca7d4c49b7ab5f45c914cc96d27466a42656
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-23 11:21:36 +0100

    42

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,6 +1,9 @@
 import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement {
+
+    selected={} //for storing answers
+
     myQuestions = [
         {
             id: "Question 1",
@@ -34,4 +37,13 @@ export default class QuizApp extends LightningElement {
         },
 
     ]
+
+    changeHandler(event) {
+        console.log("name", event.target.name)
+        console.log("value", event.target.value)
+        const { name, value } = event.target
+        //const name = event.target.name
+        //const value = event.target.value
+        this.selected = {...this.selected, [name]:value}
+    }
 }
