882d347a58faf51dfc2ce5aab18d384dd61cce85
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-24 16:00:12 +0100

    42

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -2,8 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement {
 
-    selected={} //for storing answers
-
+    selected = {} //for storing answers
+    correctAnswers = 0 //to show the correct answers
+    isSubmitted = false //use to show the result
     myQuestions = [
         {
             id: "Question 1",
@@ -38,20 +39,39 @@ export default class QuizApp extends LightningElement {
 
     ]
 
+    //used for disabling the submit button
+    get allNotSelected() {
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+    }
+
+    // for applying dynamic styling to our result
+    get isScoredFull() {
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers
+            ? 'slds-text-color_success'
+            : 'slds-text-color_error'}`
+    }
+
+    // changeHandler get's called on every click on the option
     changeHandler(event) {
-        console.log("name", event.target.name)
-        console.log("value", event.target.value)
         const { name, value } = event.target
         //const name = event.target.name
         //const value = event.target.value
-        this.selected = {...this.selected, [name]:value}
+        this.selected = { ...this.selected, [name]: value }
     }
 
-    submitHandler() {
-
+    //form submit handler
+    submitHandler(event) {
+        event.preventDefault();
+        //this.selected = {"Question 1":"a", "Question 2":"c", "Question 3":"b"}
+        let correct = this.myQuestions.filter(item => this.selected[item.id] === item.correctAnswer)
+        this.correctAnswers = correct.length
+        this.isSubmitted = true
     }
+
+    //form reset handler
     resetHandler() {
-        
+        this.selected = {}
+        this.correctAnswers = 0
+        this.isSubmitted = false
     }
-
 }
