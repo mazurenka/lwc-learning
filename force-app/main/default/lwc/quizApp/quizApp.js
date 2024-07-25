824dae0d934c5b3bb4722eecac87026e2a1ac15b
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-25 10:54:35 +0100

    43

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -9,31 +9,19 @@ export default class QuizApp extends LightningElement {
         {
             id: "Question 1",
             question: "Which one of the following is not a template loop?",
-            answers: {
-                a: "for:each",
-                b: "iterator",
-                c: "map loop"
-            },
+            answers: {a: "for:each", b: "iterator", c: "map loop"},
             correctAnswer: "c"
         },
         {
             id: "Question 2",
             question: "Which of the file is invalid in LWC component folder?",
-            answers: {
-                a: ".svg",
-                b: ".apex",
-                c: ".js"
-            },
+            answers: {a: ".svg", b: ".apex", c: ".js"},
             correctAnswer: "b"
         },
         {
             id: "Question 3",
             question: "Which one of the following is not a directive?",
-            answers: {
-                a: "for:each",
-                b: "if:true",
-                c: "@track"
-            },
+            answers: {a: "for:each", b: "if:true", c: "@track"},
             correctAnswer: "c"
         },
 
