255d9a0a55941320f7a1f754771fd65f5e12abdc
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-30 17:34:21 +0100

    53

--- force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -9,5 +9,10 @@ export default class LifeCycleParent extends LightningElement {
     connectedCallback() {
         console.log('parent connected callback called')
     }
-    
+    renderedCallback() {
+        console.log('parent rendered callback called')
+    }
+    disconnectedCallback() {
+        console.log('parent disconnected callback called')
+    }
 }
